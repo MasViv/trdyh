189a6ee89cb248a9c9ab07740fb5a76b2d88ce6a
MasViv <112710248+MasViv@users.noreply.github.com>
2023-12-27 16:35:33 -0500

    Add files via upload

---
+++ Bedroom.js
@@ -0,0 +1,47 @@
+img=""
+status="";
+
+function setup()
+{
+    canvas=createCanvas(620,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="-_-";
+}
+
+function preload()
+{
+img=loadImage("https://cdn.homedit.com/wp-content/uploads/2016/11/Small-master-bedroom-interior-design.jpg");
+}
+
+function draw()
+{
+    image(img,0,0,620,420);
+    fill("red");
+    text("doge",35,45);
+    noFill();
+    stroke("green");
+    rect(30,40,450,350);
+
+    fill("red");
+    text("cat",300,100);
+    noFill();
+    stroke("green");
+    rect(300,90,270,320);
+}
+
+function modelLoaded()
+{
+    console.log("Modal is lodad");
+    status=true;
+    objectDetector.detect(img,gotResults);
+}
+
+function gotResults(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
---
+++ Living.js
@@ -0,0 +1,47 @@
+img=""
+status="";
+
+function setup()
+{
+    canvas=createCanvas(620,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="-_-";
+}
+
+function preload()
+{
+img=loadImage("https://www.miyainteriors.com/wp-content/uploads/2020/01/LindsaySalazar.com-104.jpg");
+}
+
+function draw()
+{
+    image(img,0,0,620,420);
+    fill("red");
+    text("doge",35,45);
+    noFill();
+    stroke("green");
+    rect(30,40,450,350);
+
+    fill("red");
+    text("cat",300,100);
+    noFill();
+    stroke("green");
+    rect(300,90,270,320);
+}
+
+function modelLoaded()
+{
+    console.log("Modal is lodad");
+    status=true;
+    objectDetector.detect(img,gotResults);
+}
+
+function gotResults(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
